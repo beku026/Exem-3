044b318193d58588a8d3e148368a2c3259b07971
beku026 <beksultanbakybekov@gmail.com>
2022-06-24 01:56:50 +0600

    error corection

--- script.js
+++ script.js
@@ -1,58 +1,59 @@
-const btnOpenModal = document.querySelector(".btn--about-me");
-const modal = document.querySelector(".form");
-const modalFon = document.querySelector(".bgn-fon");
-const gridModal = document.querySelector(".grid-modal");
-const gridItem = document.querySelectorAll(".grid-item");
+const btnOpenModal = document.querySelector('.btn--about-me'),
+  modal = document.querySelector('.form'),
+  modalFon = document.querySelector('.bgn-fon'),
+  gridModal = document.querySelector('.grid-modal'),
+  gridItem = document.querySelectorAll('.grid-item');
 
-btnOpenModal.addEventListener("click", () => {
-    VisibilityOfModals(modal, modalFon, true);
+btnOpenModal.addEventListener('click', () => {
+  VisibilityOfModals(modal, modalFon, true);
 });
 
-modal.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const inputName = e.target[0].value,
+modal.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const inputName = e.target[0].value,
     inputEmail = e.target[1].value,
     inputArea = e.target[2].value;
-    const obj = {
-        name: "",
-        email: "",
-        textarea: "",
-    };
-    obj.name = inputName;
-    obj.email = inputEmail;
-    obj.textarea = inputArea;
-    console.log(obj);
-    
-    VisibilityOfModals(modal, modalFon, false);
-    e.target.reset();
+  const form = {
+    name: '',
+    email: '',
+    textarea: '',
+  };
+  form.name = inputName;
+  form.email = inputEmail;
+  form.textarea = inputArea;
+  console.log(form);
+
+  VisibilityOfModals(modal, modalFon, false);
+  e.target.reset();
 });
-modalFon.addEventListener("click", () => {
-    VisibilityOfModals(modal, modalFon, false);
+modalFon.addEventListener('click', () => {
+  VisibilityOfModals(modal, modalFon, false);
 });
 
 Array.from(gridItem).forEach((item) => {
-    item.addEventListener("click", () => {
-        const imgItem = item.children[0].src;
-            gridModal.innerHTML = `<button class="btn btn--modal-close">Close</button> 
-            <img src="${imgItem}" alt="img">`;
-        VisibilityOfModals(gridModal, modalFon, true);
+  item.addEventListener('click', () => {
+    const imgItem = item.children[0].src;
+    gridModal.innerHTML = `<button class="btn btn--modal-close">Close</button> <img src="${imgItem}" alt="img">`;
+    VisibilityOfModals(gridModal, modalFon, true);
 
-        const gridModalClose = document.querySelector(".btn--modal-close");
-        gridModalClose.addEventListener("click", () => {
-            VisibilityOfModals(gridModal, modalFon, false);
-        }); 
-        modalFon.addEventListener("click", () => {
-            VisibilityOfModals(gridModal, modalFon, false);
-        });
+    const gridModalClose = document.querySelector('.btn--modal-close');
+    gridModalClose.addEventListener('click', () => {
+      VisibilityOfModals(gridModal, modalFon, false);
     });
+    modalFon.addEventListener('click', () => {
+      VisibilityOfModals(gridModal, modalFon, false);
+    });
+  });
 });
 
 function VisibilityOfModals(modal, fon, bool) {
-    if (bool){
-        modal.style.display = "block";
-        fon.style.display = "block";
-    } else {
-        modal.style.display = "none";
-        fon.style.display = "none";
-    }
+  if (bool) {
+    modal.style.display = 'block';
+    modal.style.animation = 'modalAnimationNone 0.2s ease-in-out';
+    modal.style.animationFillMode = 'forwards';
+    fon.style.display = 'block';
+  } else {
+    modal.style.display = 'none';
+    fon.style.display = 'none';
+  }
 }
